c05ba487d7061c8cb137341d53f8dd5ce1c4984f
Teresa Bulungu Lufuluabo Pastor <teresalufu@MacBook-Pro-de-Teresa.local>
2024-04-13 00:29:47 +0200

    working: sync threads

--- philo/inc/philosophers.h
+++ philo/inc/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: telufulu <telufulu@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 17:45:25 by telufulu          #+#    #+#             */
-/*   Updated: 2024/04/11 22:36:11 by telufulu         ###   ########.fr       */
+/*   Updated: 2024/04/13 00:25:00 by telufulu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,15 +56,18 @@ typedef struct s_philo
 // main.c
 
 // routines.c
+int		check_dead(t_philo *philo, long int *time_dying);
+void	philo_eat(t_philo *philo, long int *time_dying);
+void	philo_sleep(t_philo *philo, long int *time_dying);
+void	*philo_routine(void *arg);
 
 // set_philos.c
 t_philo	*create_philos(t_config *config);
-void	*philo_routine(void *arg);
 
 // utils.c
 t_config	*get_config(char **argv);
 long int	get_time(void);
-void		*print_msg(void *arg);
+void		print_msg(t_philo *philos);
 
 // libft_utils.c
 int		ft_isspace(int c);
--- philo/srcs/routines.c
+++ philo/srcs/routines.c
@@ -6,8 +6,85 @@
 /*   By: telufulu <telufulu@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 20:43:12 by telufulu          #+#    #+#             */
-/*   Updated: 2024/04/11 20:43:35 by telufulu         ###   ########.fr       */
+/*   Updated: 2024/04/13 00:28:43 by telufulu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philosophers.h"
 
+int	check_dead(t_philo *philo, long int *time_dying)
+{
+	if (get_time() - *time_dying >= philo->config->time_die)
+	{
+		philo->flags |= DEAD;
+		philo->config->dead_flag = 1;
+		print_msg(philo);
+		return (1);
+	}
+	return (0);
+}
+
+void	philo_eat(t_philo *philo, long int *time_dying)
+{
+	print_msg(philo);
+	philo->flags |= EAT;
+	while (!(philo->flags & DEAD) && \
+		get_time() - *time_dying < philo->config->time_eat)
+		check_dead(philo, time_dying);
+}
+
+void	philo_sleep(t_philo *philo, long int *time_dying)
+{
+	print_msg(philo);
+	philo->flags |= SLEEP;
+	while (!(philo->flags & DEAD) && \
+		get_time() - *time_dying < philo->config->time_sleep + philo->config->time_eat)
+		check_dead(philo, time_dying);
+}
+
+void	philo_think(t_philo *philo)
+{
+	print_msg(philo);
+	philo->flags |= THINK;
+}
+
+void	get_forks(t_philo *philo, long int *time_dying)
+{
+	if (check_dead(philo, time_dying))
+		return ;
+	pthread_mutex_lock(&philo->fork);
+	printf("%lims %i has taken a fork\n", \
+		get_time() - philo->config->start_time, philo->num);
+	if (check_dead(philo, time_dying))
+		return ;
+	pthread_mutex_lock(&philo->prev->fork);
+	printf("%lims %i has taken a fork\n", \
+		get_time() - philo->config->start_time, philo->num);
+	if (check_dead(philo, time_dying))
+		return ;
+}
+
+void	*philo_routine(void *arg)
+{
+	t_philo		*philo;
+	long int	time_dying;
+
+	philo = (t_philo *)arg;
+	time_dying = get_time();
+	if (philo->num % 2)
+		usleep(1000);
+	while (!philo->config->dead_flag)
+	{
+		get_forks(philo, &time_dying);
+		time_dying = get_time();
+		if (!(philo->flags & DEAD) && !philo->config->dead_flag)
+			philo_eat(philo, &time_dying);
+		pthread_mutex_unlock(&philo->fork);
+		pthread_mutex_unlock(&philo->prev->fork);
+		if (!(philo->flags & DEAD) && !philo->config->dead_flag)
+			philo_sleep(philo, &time_dying);
+		if (!(philo->flags & DEAD) && !philo->config->dead_flag)
+			philo_think(philo);
+	}
+	return (0);
+}
